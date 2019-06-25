3b6674d96a404da404869e6634598fd259dae5fc
root <root@ubuntu.(none)>
2019-06-25 03:50:20 -0700

    Execve Using Stack Method

---
+++ utilities/reverse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Script to Reverse the string , printing length and showing in decimal for easy use in Stack
+import sys
+
+input = sys.argv[1]
+
+print 'String Length: ' + str(len(input))
+
+stringList = [input[i:i+4] for i in range (0, len(input), 4)]
+
+for item in stringList[::-1] :
+	print item[::-1] + ' : ' + str(item[::-1].encode('hex'))
+
